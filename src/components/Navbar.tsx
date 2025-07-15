6161c61234258e3b50c8401843e0fdafd71c2db6
diwanshu <diwanshu63019@gmail.com>
2025-07-15 23:57:38 +0530

    Fix: width, moved navItems to data file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,47 +1,14 @@
 'use client'
 
-import Image from "next/image"
 import { Container } from "./Container"
 import Link from "next/link"
 import { motion, useMotionValueEvent, useScroll } from "motion/react"
-import { CodeXml, House } from "lucide-react"
 import { useState } from "react"
 import { ThemeToggle } from "./ThemeToggle"
 import clsx from "clsx"
+import { navItems } from "@/data/navItems"
 
 export const Navbar = () => {
-    const navItems = [
-        {
-            title: 'Home',
-            href: "/",
-            icon: <House className="h-5 w-5" />,
-            external: false
-        },
-        {
-            title: 'Projects',
-            href: "/projects",
-            icon: <CodeXml className="h-5 w-5" />,
-            external: false
-        },
-        {
-            title: 'Github',
-            href: "https://github.com/DiwanshuVerma",
-            icon: <Image className="dark:invert invert-0 w-3 h-3 md:w-4 md:h-4 shrink-0 min-w-[18px] min-h-[18px]" src='/icons/github.svg' alt="github" width={20} height={20} />,
-            external: true
-        },
-        {
-            title: 'Twitter',
-            href: "https://x.com/diwanshu_28",
-            icon: <Image className="dark:invert invert-0 w-3 h-3 md:w-4 md:h-4 shrink-0 min-w-[18px] min-h-[18px]" src='/icons/x.svg' alt="twitter" width={20} height={20} />,
-            external: true
-        },
-        {
-            title: 'Linkedin',
-            href: "https://linkedin.com/in/diwanshu-verma",
-            icon: <Image className="dark:invert invert-0 w-3 h-3 md:w-4 md:h-4 shrink-0 min-w-[18px] min-h-[18px]" src='/icons/linkedin.svg' alt="linkedin" width={20} height={20} />,
-            external: true
-        },
-    ]
 
     const { scrollY } = useScroll()
     const [scrolled, setScrolled] = useState(false)
@@ -55,8 +22,8 @@ export const Navbar = () => {
             <motion.nav
                 transition={{ duration: 0.3 }}
                 className={clsx(
-                    "max-w-3xl fixed top-2 inset-x-0 z-10 mx-auto flex items-center justify-center gap-1 sm:gap-4 px-4 py-2 sm:py-3 h-12 rounded-full border shadow dark:shadow-[1px_1px_6px_0px_rgba(125,125,214,0.50)] backdrop-blur bg-white/50 dark:bg-neutral-950/50 border-neutral-300 dark:border-none text-black dark:text-white transition-all ",
-                    scrolled ? "w-[70%] md:w-[50%] duration-500" : "w-[90%] md:w-[75%] duration-500"
+                    "max-w-3xl fixed top-2 inset-x-0 z-20 mx-auto flex items-center justify-center gap-1 sm:gap-4 px-4 py-2 sm:py-3 h-12 rounded-full border shadow dark:shadow-[1px_1px_6px_0px_rgba(125,125,214,0.50)] backdrop-blur bg-white/50 dark:bg-neutral-950/50 border-neutral-300 dark:border-none text-black dark:text-white transition-all ",
+                    scrolled ? "w-[80%] md:w-[40%] duration-500" : "w-[90%] md:w-[75%] duration-500"
                 )}
             >
                 {navItems.map((item, idx) => {  
