6c4d5cd6423bfc508d9cd98d84e17172b21b962e
diwanshu <diwanshu63019@gmail.com>
2025-07-15 23:55:08 +0530

    Add: background animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { AppThemeProvider } from "./provider";
+import DotGrid from "@/components/DotgridBg";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,9 +23,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} antialiased bg-neutral-100 dark:bg-neutral-700`}
+        className={`${inter.className} relative min-h-screen antialiased bg-neutral-100 dark:bg-neutral-700`}
         suppressHydrationWarning>
         <AppThemeProvider>
+
+          {/* dots background */}
+          <div className="fixed inset-0 -z-10 pointer-events-none">
+            <DotGrid
+              dotSize={1}
+              gap={15}
+              baseColor="#5227FF"
+              activeColor="#dc2626"
+              proximity={120}
+              shockRadius={250}
+              shockStrength={5}
+              resistance={750}
+              returnDuration={1.5}
+            />
+          </div>
+
           <Navbar />
           {children}
         </AppThemeProvider>
